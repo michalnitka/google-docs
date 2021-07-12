c15fdb30c6f02e379e6c897d37b96e3ff2354958
Michał Nitka <michal.nitka.work@gmail.com>
2021-07-12 23:28:36 +0200

    Added Material Icons

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,8 @@
+import Buttom from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
 function Header() {
-  return <div></div>;
+  return <div className="flex items-center"></div>;
 }
 
 export default Header;
